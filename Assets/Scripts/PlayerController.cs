cb233ac0c4e03a86a27d8edd7883e7638ee26839
Mykola Saveliev <kolya59264@gmail.com>
2024-03-23 19:08:03 +0200

    Adding animation and fixing speed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,26 +9,33 @@ public class PlayerController : MonoBehaviour
 {
 	private Rigidbody rb;
 	private Player player;
-	public bool onGround = true;
-	public bool canJump = true;
+	public bool onGround = false;
+	public bool canJump = false;
 
-	public float jumpForce = 1500.0f;
-	public float jump;
-	
+	[Range(0f, 100.0f)]
+	public float jumpForce = 25.0f;
+	public float jump = 200.0f;
+	public float fallMultiplier = 2.5f;
+	public float jumpMultiplier = 2.0f;
+
+	private Animator animator = null;
+
 	// Start is called before the first frame update
 	private void Awake()
 	{
 		player = GetComponent<Player>();
 		rb = GetComponent<Rigidbody>();
-		rb.velocity = new Vector3(player.speed / 4, -100);
-		jump = 500;
+		rb.velocity = new Vector3(player.speed / 10, Physics.gravity.y * (fallMultiplier - 1));
+		animator = player.GetComponent<Animator>();
 	}
 
 	private void Update()
 	{
 		// TODO: fix speed
-		if (!onGround)
-			rb.velocity = new Vector3(player.speed / 4, -100);
+		if (!onGround && rb.velocity.y < 0)
+			rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+		else
+			rb.velocity = new Vector3(player.speed / 10, 0);
 		if (Input.GetButtonDown("Jump") && onGround)
 		{
 			canJump = true;
@@ -38,6 +45,22 @@ public class PlayerController : MonoBehaviour
 	private void OnCollisionEnter(Collision collision)
 	{
 		Debug.Log("collider is " + collision.collider.tag);
+		if (collision.collider.CompareTag("TopBlock"))
+		{
+			onGround = true;
+			rb.velocity = new Vector3(player.speed / 7, 0);
+			animator.CrossFade("PlayerRun", 0);
+
+			// TODO: set idle animation when player collides to wall
+			if (rb.velocity.x <= 0)
+			{
+				animator.CrossFade("Idle", 0);
+			}
+		}	
+	}
+
+	private void OnCollisionStay(Collision collision)
+	{
 		if (collision.collider.CompareTag("TopBlock"))
 			onGround = true;
 	}
@@ -55,9 +78,25 @@ public class PlayerController : MonoBehaviour
 	{
 		if (canJump && onGround)
 		{
-			rb.velocity = new Vector3(0, (jumpForce * 2), 0);
+			//rb.AddForce(new Vector3(rb.velocity.x * jumpForce, jump * jumpForce, 0), ForceMode.Impulse);
+			Jump(animator);
+			//rb.velocity = new Vector3(player.speed, jumpForce, 0);
 			onGround = false;
 			canJump = false;
 		}
 	}
+
+	private void Jump(Animator animator)
+	{
+		if (animator)
+		{
+			animator.CrossFade("Jump", 0);
+		}
+		else
+		{
+			Debug.LogError("No animator found!");
+		}
+		rb.velocity = new Vector3(0, jump * jumpForce, 0);
+		rb.velocity += Vector3.up * Physics.gravity.y * (jumpMultiplier - 1) * Time.deltaTime;
+	}	
 }
